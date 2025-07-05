0bdba5a5fbc4f3d8672be5ef28004e24ccf4c152
Chandrashekar185 <chanduminny32@gmail.com>
2025-07-05 21:56:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 function manipulateArray(){
-return new Promsie((resolve)=>{
-	setTimeout(()=>{
-resolve([1,2,3,4]);},3000);});
+return new Promise((resolve)=>{
+setTimeout(()=>{
+resolve([1,2,3,4]);},3000);};)}
 .then((array)=>{
-const evenNumbers=array.filter((num)=>num%2===0);
 setTimeout(()=>{
-documnet.getElementById("output").innerHTML=evenNumbers;},
-1000);
-return evenNumbers;
-})
-.then((evenNumbers)=>{const multiply=evenNumbers.map((num)=> num*2);
+const evenNumbers=array.filter(num=>num%2===0);
+document.getElementById("output").textContent=evenNumbers.join(',');}
+,1000);
+return evenNumbers;});
+.then((evenNumber)=>{
 setTimeout(()=>{
-document.getElementById("output").innerHTMl=multiply;},
-		   2000);
-	
+	evenNumber.map(num=>num*2);
+	document.getElementById("output").textcontent=evenNumbers.join(','));}
+		   ,2000);
 });
-}
 manipulateArray();
+
