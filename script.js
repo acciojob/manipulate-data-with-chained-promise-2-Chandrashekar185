faefc3650a2c22e927d3d38b5be55fc04c768c65
Chandrashekar185 <chanduminny32@gmail.com>
2025-07-05 16:33:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+function manipulateArray(){
+return new Promsie((resolve)=>{
+	setTimeout(()=>{
+resolve([1,2,3,4]);},3000);});
+.then((array)=>{
+const evenNumbers=array.filter((num)=>num%2===0);
+setTimeout(()=>{
+documnet.getElementById("output").innerHTML=evenNumbers;},
+1000);
+return evenNumbers;
+})
+.then((evenNumbers)=>{const multiply=evenNumbers.map((num)=> num*2);
+setTimeout(()=>{
+document.getElementById("output").innerHTMl=multiply;},
+		   2000);
+	
+});
+}
+manipulateArray();
