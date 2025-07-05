cf438b3c63a81799739e14e4b0457a3231e4a421
Chandrashekar185 <chanduminny32@gmail.com>
2025-07-05 22:13:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-function manipulateArray(){
-return new Promise((resolve)=>{
-setTimeout(()=>{
-resolve([1,2,3,4]);},3000);};)}
-.then((array)=>{
-setTimeout(()=>{
-const evenNumbers=array.filter(num=>num%2===0);
-document.getElementById("output").textContent=evenNumbers.join(',');}
-,1000);
-return evenNumbers;});
-.then((evenNumber)=>{
-setTimeout(()=>{
-	evenNumber.map(num=>num*2);
-	document.getElementById("output").textcontent=evenNumbers.join(','));}
-		   ,2000);
-});
-manipulateArray();
+function manipulateArray() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([1, 2, 3, 4]);
+        }, 3000);
+    })
+    .then(array => {
+        const evenNumbers = array.filter(num => num % 2 === 0);
+        setTimeout(() => {
+            document.getElementById('output').innerHTML = evenNumbers.join(',');
+        }, 1000);
 
+        return evenNumbers; 
+    })
+    .then(evenNumbers => {
+        const multiplied = evenNumbers.map(num => num * 2);
+        setTimeout(() => {
+         document.getElementById('output').innerHTML = multiplied.join(',');
+        }, 2000);
+    });
+}
+manipulateArray();
